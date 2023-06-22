6d7e504cc8df7e3ab479fbb39e52e036a0570031
Đorđe Jocić <office@djordjejocic.com>
2023-06-22 22:58:03 +0200

    Generate WAVE binaries - For Experimenting

--- doodles/2023/audio-sampling/src/main.cpp
+++ doodles/2023/audio-sampling/src/main.cpp
@@ -3,10 +3,10 @@
 #define ADC_REFERENCE_V           3.3F
 #define ADC_RESOLUTION            10
 #define ADC_SAMPLING_RATE         5000
-#define ADC_SAMPLING_DURATION_SEC 10
+#define ADC_SAMPLING_DURATION_SEC 25
 
 #define SINE_FREQUENCY_HZ 500
-#define SINE_VPP_MV       200
+#define SINE_VPP_MV       400
 #define SINE_DC_OFFSET_V  1.65F
 
 #include <iostream>
@@ -14,17 +14,21 @@
 #include <cstdint>
 #include <cmath>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 const uint8_t WAV_HEADER[44] = {
-    0x52, 0x49, 0x46, 0x46, 0xC4, 0x86, 0x01, 0x00, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x88, 0x13, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x02, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 0xA0, 0x86
+    0x52, 0x49, 0x46, 0x46, 0xC4, 0x86, 0x01, 0x00, 0x57, 0x41, 0x56, 0x45,
+    0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00,
+    0x88, 0x13, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x02, 0x00, 0x10, 0x00,
+    0x64, 0x61, 0x74, 0x61, 0xA0, 0x86
 };
 
 uint16_t* generate_samples(uint16_t& size) {
     
-    uint16_t samples_total = ADC_SAMPLING_RATE * ADC_SAMPLING_DURATION_SEC;
-    uint16_t adc_range     = pow(2, ADC_RESOLUTION);
+    uint32_t samples_total = ADC_SAMPLING_RATE * ADC_SAMPLING_DURATION_SEC;
+    uint32_t adc_range     = pow(2, ADC_RESOLUTION);
     double   volts_per_bit = ADC_REFERENCE_V / adc_range;
     
     uint16_t* samples = new uint16_t[samples_total];
@@ -44,7 +48,7 @@ uint16_t* generate_samples(uint16_t& size) {
     int j = 0;
     
     for (float radian = 0; radian < 2 * PI_VAL; radian += radian_increment) {
-        period_samples[j++] = (sin(radian) * ((SINE_VPP_MV / 1e3) / volts_per_bit)) + (SINE_DC_OFFSET_V / volts_per_bit);
+        period_samples[j++] = min(uint16_t((sin(radian) * ((SINE_VPP_MV / 1e3) / volts_per_bit)) + (SINE_DC_OFFSET_V / volts_per_bit)), adc_range);
     }
     
     j = 0;
@@ -85,30 +89,59 @@ void generate_matlab_array(uint16_t* data, uint16_t& size) {
     cout << " ];" << endl;
 }
 
+vector<uint8_t> generate_wave_binaries(uint16_t* data, uint16_t& size) {
+    
+    vector<uint8_t> bin;
+    
+    for (int i = 0; i < 44; i++) {
+        bin.push_back(WAV_HEADER[i]);
+    }
+    
+    for (uint32_t i = 0; i < size; i++) {
+        
+        int16_t test = data[i] - 1024;
+        
+        bin.push_back((test >> 8) & 0xFF);
+        bin.push_back(test & 0xFF);
+    }
+    
+    uint32_t rate = ADC_SAMPLING_RATE;
+    
+    bin[24] = (rate & 0xFF);
+    bin[25] = ((rate >> 8) & 0xFF);
+    bin[26] = ((rate >> 16) & 0xFF);
+    bin[27] = ((rate >> 24) & 0xFF);
+    
+    uint32_t byte_rate = ADC_SAMPLING_RATE * 1 * 2;
+    
+    bin[28] = (byte_rate & 0xFF);
+    bin[29] = ((byte_rate >> 8) & 0xFF);
+    bin[30] = ((byte_rate >> 16) & 0xFF);
+    bin[31] = ((byte_rate >> 24) & 0xFF);
+    
+    uint32_t subchunk_size = size * 1 * 2;
+    
+    bin[40] = (subchunk_size & 0xFF);
+    bin[41] = ((subchunk_size >> 8) & 0xFF);
+    bin[42] = ((subchunk_size >> 16) & 0xFF);
+    bin[43] = ((subchunk_size >> 24) & 0xFF);
+    
+    return bin;
+}
+
 int main() {
     
     uint16_t  sample_size;
     uint16_t* sample_data = generate_samples(sample_size);
     
-    //generate_matlab_array(sample_data, sample_size);
+    generate_matlab_array(sample_data, sample_size);
     
-    ofstream of;
+    vector<uint8_t> binary_wave = generate_wave_binaries(sample_data, sample_size);
     
-    of.open("test.wav", ios_base::out | ios_base::binary);
-    cout << sample_size << endl;
-    if (of.is_open()) {
-        of.write((char*)WAV_HEADER, 44);
-        
-        for (int i = 0; i < sample_size; i++) {
-            
-            int test = sample_data[i] - 1024;
-            
-            of << (unsigned char)((test >> 8) & 0xFF);
-            of << (unsigned char)(test & 0xFF);
-            
-        }
-        
-        of.seekp(4);
+    ofstream output("sine.wav", ios_base::binary | ios_base::out);
+    
+    for (auto n : binary_wave) {
+        output << n;
     }
     
     return 0;
