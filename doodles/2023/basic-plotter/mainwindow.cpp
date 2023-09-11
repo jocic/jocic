1f05d2dca48af6acc6853f51d39cf08a2e712fc1
Đorđe Jocić <office@djordjejocic.com>
2023-09-11 03:27:39 +0200

    Basic Plotter - Doodle

---
+++ doodles/2023/basic-plotter/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include <QHBoxLayout>
+
+#include "mainwindow.h"
+#include "basic_plot.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    
+    auto plot_buffer = ui->plot->get_Buffer();
+    
+    plot_buffer->append({ 0, 0 });
+    plot_buffer->append({ 50, 100 });
+    plot_buffer->append({ 100, 500 });
+    plot_buffer->append({ 300, 300 });
+    plot_buffer->append({ 400, 800 });
+    plot_buffer->append({ 800, 3000 });
+    plot_buffer->append({ 3800, 2000 });
+    plot_buffer->append({ 4000, 4000 });
+    
+    ui->plot->refresh();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
